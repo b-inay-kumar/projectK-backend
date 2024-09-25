ef4a642021324bf4564024c75b71b0f3cad1e859
binay <binay@skentino.com>
2024-09-25 18:08:51 +0530

    added contactus api

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,11 @@
-//index Route of Backend
+// import express from "express";
+// import { requestForContact } from "../controller/ContactController";
+const express = require('express')
+const { requestForContact,viewContact } = require('../controller/ContactController');
+
+const router = express.Router();
+
+router.post("/contact", requestForContact); 
+router.get("/contact", viewContact);
+
+module.exports = router
